49c4029465481f0f01f703c15e930b3d25a8181c
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-03-29 11:27:31 +0100

    add variants to hero for styling

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -2,21 +2,23 @@ import styles from './hero.module.css'
 // import image from '../../../src/assets/images/Frame-23.svg'
 import YellowButton from '../buttons/yellowButton'
 
-const Hero = (props) =>  {
+const Hero = ({ heroHead, heroText, image, variant }) =>  {
     return(
         <>
-            <div className={styles.heroContainer}>
+            <div className={styles.heroContainer} variantWhite={variant}>
                 
                 <section className={styles.heroHeadingContainer}>
-                    <h2 className={styles.heroHeading}>{props.heroHeading}</h2>
-                    <p className={styles.heroText}>{props.heroText}</p>
-                    <YellowButton yellowBtn="Get Started" variant="left">
-                        <p>Get started</p>
-                    </YellowButton>
-                </section>
+                    <h2 className={styles.heroHeading}>{heroHead}</h2>
+                    <p className={styles.heroText}>{heroText}</p>
+                    <div className={styles.yellowButtonContainer} variantWhite={variant}>
+                        <YellowButton yellowBtn="Get Started" variant="left">
+                            <p>Get started</p>
+                        </YellowButton>
+                    </div>
+                </section >
 
                 <section className={styles.heroImageContainer}>
-                    <img src={props.image} alt="Image of a man carring an open laptop on his hand" className={styles.heroImage}/>
+                    <img src={image} alt="Image of a man carring an open laptop on his hand" className={styles.heroImage}/>
                     
                 </section>
 
