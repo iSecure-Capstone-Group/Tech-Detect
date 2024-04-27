b8e96f3ab9ef1deaba1c735dc130571962449808
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-04-27 05:05:14 +0100

    add  hamburger icon to dashboard

--- src/pages/dashboard/searchAndProfilePic.jsx
+++ src/pages/dashboard/searchAndProfilePic.jsx
@@ -3,19 +3,9 @@ import { LoginSignupValidation } from "../../components/forms/loginSignupFormVal
 import ProfilePicture from "./profilePicture"
 import hamgurgerStyles from "../../components/navigation/navigation.module.css"
 import { useState } from "react"
-// import DashboardNavModal from "./dashboardNavModal"
+import DashboardNavModal from "./dashboardNavModal"
 
 const SearchAndProfilePic = () => {
-
-    // const [showModal, setShowModal] = useState(false);
-
-    // const openModal = () => {
-    //   setShowModal(true);
-    // };
-  
-    // const closeModal = () => {
-    //   setShowModal(false);
-    // };
     return(
         <>
             <div className={styles.allSearchAndPic}>
@@ -45,12 +35,9 @@ const SearchAndProfilePic = () => {
                     
                 </div>
                 <div className={`${hamgurgerStyles.hamburgerMenuIcon} ${styles.dashboardHamburgerIcon}`}>
-                        <svg width="25" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.234 12h18M3.234 6h18M3.234 18h18" stroke="#0D0D0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                <DashboardNavModal />
                 </div>
-                {/* <div className={styles.hamburgerMenuIcon} onClick={openModal}>
-                    <svg width="25" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.234 12h18M3.234 6h18M3.234 18h18" stroke="#0D0D0D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
-                </div>
-                {showModal && <DashboardNavModal onClose={closeModal} />} */}
+                
             </div>
 
             
