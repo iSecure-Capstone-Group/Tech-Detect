8e345e8cd3542d1c5d4f02305226b6f04d8345f7
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-20 12:10:26 +0100

    create `.env` file, move key/value to the file

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXCfOQf4sQtKC5EHVgznjKkOayR9foCao",
-  authDomain: "techdetect-31c74.firebaseapp.com",
-  projectId: "techdetect-31c74",
-  storageBucket: "techdetect-31c74.appspot.com",
-  messagingSenderId: "480948058361",
-  appId: "1:480948058361:web:c04a07c5afbcc375f80a28",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
