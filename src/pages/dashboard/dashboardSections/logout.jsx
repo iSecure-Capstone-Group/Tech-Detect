fde8ac891fa2a549d6f6047a738f46ac15394ceb
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-20 02:56:55 +0100

    implement logout functionality

--- src/pages/dashboard/dashboardSections/logout.jsx
+++ src/pages/dashboard/dashboardSections/logout.jsx
@@ -14,7 +14,7 @@
 
 // export default Logout
 
-
+import { useState, useEffect } from 'react';
 import styles from "./logout.module.css"
 import LogoOnly from "../../../components/logoOnly"
 import { Link } from "react-router-dom"
@@ -25,41 +25,75 @@ import Dashboard from "../dashboard"
 import logoutSuccess from "../../../components/modals/logoutSuccess"
 import LogoutSuccess from "../../../components/modals/logoutSuccess"
 import resetStyles from "../../../components/modals/modals.module.css"
-const Logout = () => {
-    return(
-        <>
-            <div className={styles.logoutContainer}>
-                
-                <div className={styles.logout}>
-                    <div className={formContainerStyles.modalLogo}>
-                        <LogoOnly />
-                    </div>
-                    <p className={`${formContainerStyles.fillForm} ${styles.formInstruction}`}>Are you sure you want to Logout?</p>
-                    <LogoutSuccess
-                                    modalContent={[
-                                        <div className={resetStyles.contents}>
-                                            <div key="1"><LogoOnly /></div>,
-                                            <p key="2"><svg width="121" height="121" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M60.5 110.5c27.5 0 50-22.5 50-50s-22.5-50-50-50-50 22.5-50 50 22.5 50 50 50Z" stroke="#007300" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M39.25 60.5 53.4 74.65l28.35-28.3" stroke="#007300" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg></p>,
-                                            <h5 key="3">Logout Successful</h5>,
-                                            <p>You have successfully logged out from your account</p>,
-                                            <div key="4">
-                                                <YellowButton yellowBtn="Get Started" variant="long" >
-                                                    <Link to="/loginPage">Login</Link>
-                                                </YellowButton>
-                                            </div>,
-                                        </div>
-                                        // Add more elements as needed
-                                    ]}
-                                />
-                    {/* <YellowButton yellowBtn="Get Started" variant="long" >
-                        <Link to="/loginPage">Get Started</Link>
-                    </YellowButton> */}
+import { auth } from "../../../firebase"
 
-                    <Link to="/dashboard" className={`${backstyles.bacButton} ${styles.backLink}`}>No, Take me Back</Link>
-                </div>
-                
-            </div>
-        </>
+const Logout = () => {
+    const [userDetails, setUserDetails] = useState(null);
+  // const [settingsData, setSettingsData] = useState(null);
+  const fetchUserData = async () => {
+    auth.onAuthStateChanged(async (user) => {
+      console.log(user)
+      setUserDetails(user)
+    })
+  }
+
+
+    async function handleLogout(){
+        try{
+            await auth.signOut()
+            window.location.href = "../login"
+            alert("Logout Successful")
+           
+        }catch (error) {
+            console.log("Error logging out:", error.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchUserData();
+      }, []);
+
+    return(
+       <div>
+        {userDetails ? (
+             <>
+             <div className={styles.logoutContainer}>
+                 
+                 <div className={styles.logout}>
+                     <div className={formContainerStyles.modalLogo}>
+                         <LogoOnly />
+                     </div>
+                     <p className={`${formContainerStyles.fillForm} ${styles.formInstruction}`}>Are you sure you want to Logout?</p>
+                     {/* <LogoutSuccess
+                                     modalContent={[
+                                         <div className={resetStyles.contents}>
+                                             <div key="1"><LogoOnly /></div>,
+                                             <p key="2"><svg width="121" height="121" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M60.5 110.5c27.5 0 50-22.5 50-50s-22.5-50-50-50-50 22.5-50 50 22.5 50 50 50Z" stroke="#007300" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M39.25 60.5 53.4 74.65l28.35-28.3" stroke="#007300" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg></p>,
+                                             <h5 key="3">Logout Successful</h5>,
+                                             <p>You have successfully logged out from your account</p>,
+                                             <div key="4">
+                                                 <YellowButton yellowBtn="Get Started" variant="long" >
+                                                     <Link to="/loginPage">Login</Link>
+                                                 </YellowButton>
+                                             </div>,
+                                         </div>
+                                         // Add more elements as needed
+                                     ]}
+                                 /> */}
+                     {/* <YellowButton yellowBtn="Get Started" variant="long" >
+                         <Link to="/loginPage">Get Started</Link>
+                     </YellowButton> */}
+                     <YellowButton yellowBtn="Get Started" variant="long" >
+       <button onClick={handleLogout}>Yes, Proceed to Logout </button>
+       </YellowButton>
+ 
+                     <Link to="/dashboard" className={`${backstyles.bacButton} ${styles.backLink}`}>No, Take me Back</Link>
+                 </div>
+                 
+             </div>
+         </>
+        ): <p>Loading...</p>}
+       </div>
     )
 }
 
